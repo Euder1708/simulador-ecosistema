89a8cbc88c39e72640b784d551c03aff81d5ad4b
Euder <ejpachecoa@ufpso.edu.co>
2024-10-05 16:26:29 -0500

    (doc)Readme:Hice las correciones al codigo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Aquí dejarás tu documentación, por qué decidiste realizar esas optimización y cómo resolvistes los ejercicios de lógica...
+eliminarè algunos valopres magicos, los iuntroducirè en la variable NUMERO_DIAS
+Defini otras dos variables para las probabilidades las reemplaze porque eran valores màgicos 
+Remplaso por variables todos los valores agicos que hay 
+luego cambie los if por un switch para reducir el codigo
--- src/SimuladorEcosistema.java
+++ src/SimuladorEcosistema.java
@@ -4,24 +4,29 @@ public class SimuladorEcosistema {
     public static void main(String[] args) {
         char[][] ecosistema = new char[20][20];
         Random random = new Random();
-
+         final int NUMERO_DIAS=20;
+         final double NUMERO1_PROBABILIDAD=0.1;
+         final double NUMERO2_PROBABILIDAD=0.2;
+         final int NUMERO1_VECINOS =2;
+         final int NUMERO2_VECINOS=3;
+         final double NUMERO3_VECINOS=0.01;
         // Inicializar ecosistema
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 20; j++) {
+        for (int i = 0; i < NUMERO_DIAS; i++) {
+            for (int j = 0; j < NUMERO_DIAS; j++) {
                 double prob = random.nextDouble();
-                if (prob < 0.1) ecosistema[i][j] = 'A'; // Árbol
-                else if (prob < 0.2) ecosistema[i][j] = 'H'; // Hierba
+                if (prob < NUMERO1_PROBABILIDAD) ecosistema[i][j] = 'A'; // Árbol
+                else if (prob < NUMERO2_PROBABILIDAD) ecosistema[i][j] = 'H'; // Hierba
                 else ecosistema[i][j] = ' '; // Vacío
             }
         }
 
         // Simulación por 20 días
-        for (int dia = 1; dia <= 20; dia++) {
+        for (int dia = 1; dia <= NUMERO_DIAS; dia++) {
             System.out.println("Día " + dia);
             
             // Mostrar ecosistema
-            for (int i = 0; i < 20; i++) {
-                for (int j = 0; j < 20; j++) {
+            for (int i = 0; i < NUMERO_DIAS; i++) {
+                for (int j = 0; j < NUMERO_DIAS; j++) {
                     System.out.print(ecosistema[i][j] + " ");
                 }
                 System.out.println();
@@ -29,36 +34,41 @@ public class SimuladorEcosistema {
 
             // Actualizar ecosistema
             char[][] nuevoEcosistema = new char[20][20];
-            for (int i = 0; i < 20; i++) {
-                for (int j = 0; j < 20; j++) {
+            for (int i = 0; i < NUMERO_DIAS; i++) {
+                for (int j = 0; j < NUMERO_DIAS; j++) {
                     // Contar vecinos (integrado directamente aquí)
                     int vecinos = 0;
                     for (int di = -1; di <= 1; di++) {
                         for (int dj = -1; dj <= 1; dj++) {
                             if (di == 0 && dj == 0) continue;
-                            int ni = (i + di + 20) % 20;
-                            int nj = (j + dj + 20) % 20;
+                            int ni = (i + di + NUMERO_DIAS) % NUMERO_DIAS;
+                            int nj = (j + dj + NUMERO_DIAS) % NUMERO_DIAS;
                             if (ecosistema[ni][nj] == 'A') vecinos++;
                         }
                     }
 
-                    if (ecosistema[i][j] == 'A') {
-                        if (vecinos < 2 || vecinos > 3) nuevoEcosistema[i][j] = 'H';
-                        else nuevoEcosistema[i][j] = 'A';
-                    } else if (ecosistema[i][j] == 'H') {
-                        if (vecinos == 3) nuevoEcosistema[i][j] = 'A';
-                        else nuevoEcosistema[i][j] = 'H';
-                    } else {
-                        if (random.nextDouble() < 0.01) nuevoEcosistema[i][j] = 'H';
-                        else nuevoEcosistema[i][j] = ' ';
+                    switch (ecosistema[i][j]) {
+                        case 'A' -> {
+                            if (vecinos < NUMERO1_VECINOS || vecinos > NUMERO2_VECINOS) nuevoEcosistema[i][j] = 'H';
+                            else nuevoEcosistema[i][j] = 'A';
+                        }
+                        case 'H' -> {
+                            if (vecinos == NUMERO2_VECINOS) nuevoEcosistema[i][j] = 'A';
+                            else nuevoEcosistema[i][j] = 'H';
+                        }
+                        default -> {
+                            if (random.nextDouble() < NUMERO3_VECINOS) nuevoEcosistema[i][j] = 'H';
+                            else nuevoEcosistema[i][j] = ' ';
+                        }
                     }
                 }
             }
             ecosistema = nuevoEcosistema;
+            final int millas=1000;
 
             // Pausa entre días
             try {
-                Thread.sleep(1000);
+                Thread.sleep(millas);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
